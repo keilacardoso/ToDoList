6024a0480df3a5b9b8e59d80ffeb58591fbbba4b
keilacardodo <keila_dcardoso@hotmail.com>
2021-07-01 15:56:05 -0300

    opcao ajax tarefa automatica coment

--- js/application.js
+++ js/application.js
@@ -5,15 +5,15 @@ const tasks = document.getElementById('tasks');
 
 let count = 0;
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(json => {
-        json.forEach(toDo =>{
-            if(toDo.completed === false){
-            addTask(toDo.completed, toDo.title);
-            }
-        });
-
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then(response => response.json())
+//     .then(json => {
+//         json.forEach(toDo =>{
+//             if(toDo.completed === false){
+//             addTask(toDo.completed, toDo.title);
+//             }
+//         });
+//     });
 
 form_task.addEventListener('submit', function(event){
 
@@ -60,16 +60,3 @@ function addTask(status, digitedTask){
 tasks.innerHTML += text_task;
 
 }
-
-
-//     const text_task = `
-//         <li class="list-group-item">
-//             <div class="form-check d-flex justify-content-between align-items-center">
-//                 <input class="form-check-input" type="checkbox" id="task_${count}">
-//                 <label class="form-check-label flex-grow-1 ms-2" for="task_${count}">${input_task.value}</label>
-//                 <button class="btn btn-danger btn-sm" id="cancel_btn" onclick="toDelete(this);"> Deletar </button>
-//             </div>
-//         </li>
-// `;
-// tasks.innerHTML += text_task;
-
